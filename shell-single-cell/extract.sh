434ac91df73f634be398b3eb3a3ae60d380c9b51
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-11-14 02:23:07 +0800

    Update extract.sh

--- shell-single-cell/extract.sh
+++ shell-single-cell/extract.sh
@@ -7,9 +7,18 @@ cd ./clean
 # > fastq.txt
 
 # Loop through all .gz files in the directory
+# pair-end
 for file in *.gz
 do
   # 提取下划线前的部分并输出到 fastq.txt
   echo "${file%%_*}"
+done | sort | uniq > ../fastq.txt
+
+
+# single-end
+for file in *.gz
+do
+  # 提取下划线前的部分并输出到 fastq.txt
+  echo "${file%%.*}"
 done | sort | uniq > ../fastq.txt
 
