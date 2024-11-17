c5157db86351b076471f9ac08b61c0b6863edc12
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-11-18 04:03:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # url='https://github.com/1027victor/pre_MARVEL',
     entry_points={
         'console_scripts': [
-            'merge-bed=pre_marvel.merge_bed:full_process',
+            'merge-bed=pre_marvel.merge_bed:process_and_combine_counts',
             'merge-counts=pre_marvel.merge_counts:run_pipeline',
             'merge-SJ=pre_marvel.merge_SJ:process_files'
         ]
