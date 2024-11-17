b4613c1f549827136ae11da9c881b758c4ea1f06
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-11-18 04:03:07 +0800

    Update __init__.py

--- pre_marvel/__init__.py
+++ pre_marvel/__init__.py
@@ -1,3 +1,3 @@
-from .merge_bed import full_process as merge_bed
+from .merge_bed import process_and_combine_counts as merge_bed
 from .merge_counts import run_pipeline as merge_counts
 from .merge_SJ import  process_files as merge_SJ
