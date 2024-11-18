36374658527143da8a23adf09a9a23df8e1496cf
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-11-18 17:28:10 +0800

    Update bed.sh

--- shell-single-cell/bed.sh
+++ shell-single-cell/bed.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
   
-# 指定bedtools的基因组文件和BED文件
+# Specify the genomic file and BED file for bedtools
 genome_file="/www/hpw/data/rstudio/PRJEB30442_human/all_bam/chrNameLength.txt"
 bed_file="/www/hpw/data/rstudio/PRJEB30442_human/all_bam/unsortbam/rmats/ri.coordinates.bed"
 
-# 检查bedtools是否已安装
+# Check if bedtools is already installed
 if ! command -v bedtools &> /dev/null
 then
     echo "bedtools could not be found"
     exit
 fi
 
-# 循环处理所有符合条件的.bam文件
+# Iterate through all the eligible .bam files
 for bam_file in *.Aligned.sortedByCoord.out.bam
 do
-    # 生成输出文件的名称
+    # Generate the name of the output file
     output_file="./bed/${bam_file%.Aligned.sortedByCoord.out.bam}.txt"
 
     echo "Processing $bam_file -> $output_file"
 
-    # 执行bedtools coverage命令
+    # Execute the bedtools coverage command
     bedtools coverage \
         -g "$genome_file" \
         -split \
