59759617bd91e13a7a2b57347616864a6ef194f2
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-11-18 16:40:45 +0800

    Update SJ.sh

--- shell-single-cell/SJ.sh
+++ shell-single-cell/SJ.sh
@@ -1,15 +1,15 @@
-# 提取SJ.out.tab文件特定的列数且合并坐标列
+# Extract a specific number of columns from the SJ.out.tab file and merge the coordinate columns
 #!/bin/bash
 mkdir -p SJ
-# 遍历当前目录下所有以.SJ.out.tab结尾的文件
+# Iterate through all files in the current directory that end with ".SJ.out.tab"
 for file in *.SJ.out.tab
 do
-    # 使用basename命令提取文件名（不带扩展名）
+    #  Use the "basename" command to extract the file name (without the extension)
     filename=$(basename "$file" .SJ.out.tab)
 
-    # 首先，使用awk命令处理文件，添加标题并提取所需列
-    # 然后，对这个输出应用第二次awk处理，合并染色体位置信息并保留最后一列
-    # 直接将最终结果输出到以"_merge.txt"结尾的文件中
+    # First, use the awk command to process the file, add a header and extract the required columns
+    # Then, apply a second awk processing to this output to merge the chromosome position information and retain the last column
+    # Directly output the final result to a file ending with "_merge.txt
     awk -v OFS='\t' -v colname="$filename" 'BEGIN {print "chr", "start", "end", colname} {print $1, $2, $3, $7}' "$file" | awk -v OFS='\t' 'NR==1{print "coord.intron", $4; next} {print $1":"$2":"$3, $4}' > "./SJ/${filename}.txt"
 done
 
